720d019ad85d13d285a92d6804d799875ddb0dae
Andy <obrezkov.andy@gmail.com>
2025-05-17 23:13:39 +0300

    Update Task2 and tests

--- Exam_2/Task2.cs
+++ Exam_2/Task2.cs
@@ -7,12 +7,12 @@ using CW_2.Var_00;
 
 namespace CW2.Var00
 {
-    public class Task2 : ITask1
+    public class Task2 : ITask2
     {
         private string _input;
-        private int _output;
+        private string _output;
         public string Input => _input;
-        public int Output => _output;
+        public string Output => _output;
         
         public Task2(string text)
         {
@@ -22,15 +22,35 @@ namespace CW2.Var00
             var symbols = new char[]
                 { '.', '!', '?', ',', ':', '\'', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' ' };
 
-            var sentences = Input.Split(endSymbols);
+            var sentences = new List<string>();
+            var sb = new StringBuilder();
+            for (int i = 0; i < Input.Length; i++)
+            {
+                sb.Append(Input[i]);
+
+                if (endSymbols.Contains(Input[i]))
+                {
+                    while (i + 1 < Input.Length && endSymbols.Contains(Input[i + 1]))
+                    {
+                        sb.Append(Input[i + 1]);
+                        i++;
+                    }
+                    
+                    sentences.Add(sb.ToString().Trim());
+                    sb.Clear();
+                }
+            }
+            
             int max = 0;
             foreach (var sentence in sentences)
             {
                 var words = sentence.Split(symbols).Where(x => x != "").ToArray();
-                max = Math.Max(max, words.Length);
+                if (words.Length > max)
+                {
+                    max = words.Length;
+                    _output = sentence;
+                }
             }
-
-            _output = max;
         }
 
         public override string ToString()
